eeb4666c04d782d896095a27012c37188b82a9c7
Soumyabrata1994 <soumyabrata.bhattacharyya@gmail.com>
2018-09-11 17:45:07 +0530

    1st push

---
+++ src/com/hellocontroller/HelloController.java
@@ -0,0 +1,35 @@
+package com.hellocontroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ * Servlet implementation class HelloController
+ */
+//@WebServlet("/HelloController")
+@Controller
+public class HelloController //extends AbstractController {
+{
+	/*protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception{
+		
+		ModelAndView md = new ModelAndView("HelloPage");
+		md.addObject("welcomeMessage","Hi Welcome to first Spring MVC");
+		return md;
+	}*/
+	public ModelAndView helloWorld(){
+		ModelAndView md = new ModelAndView("HelloPage");
+		md.addObject("welcomeMessage","Hi Welcome to first Spring MVC");
+		return md;
+	}
+	
+	
+}
